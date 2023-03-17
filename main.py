a87813fdcb7ece930aaf83f50ce21c77acfe913a
Андрій <andrey.maybrodskyy@gmail.com>
2023-03-17 14:23:05 +0200

    change for author + adapter and pipe

--- main.py
+++ main.py
@@ -3,46 +3,81 @@ import datetime
 from models import Author, Quotes
 import scrapy
 from scrapy.crawler import CrawlerProcess
+from scrapy.item import Item, Field
+from itemadapter import ItemAdapter
+
+
+class QuoteItem(Item):
+    keywords = Field()
+    author = Field()
+    quote = Field()
+
+
+class AuthorItem(Item):
+    fullname = Field()
+    date_born = Field()
+    location_born = Field()
+    description = Field()
+
+
+class Q_Pipline:
+    quotes = []
+    authors = []
+
+    def process_item(self, item, spider):
+        adapter = ItemAdapter(item)
+        if 'fullname' in adapter.keys():
+            self.authors.append({
+                "fullname": adapter["fullname"],
+                "date_born": adapter["date_born"],
+                "location_born": adapter["location_born"],
+                "description": adapter["bio"],
+            })
+        if 'quote' in adapter.keys():
+            self.quotes.append({
+                "keywords": adapter["keywords"],
+                "author": adapter["author"],
+                "quote": adapter["quote"],
+            })
+        return
+
+    def close_spider(self, spider):
+        with open('quotes.json', 'w', encoding='utf-8') as fd:
+            json.dump(self.quotes, fd, ensure_ascii=False)
+        with open('authors.json', 'w', encoding='utf-8') as fd:
+            json.dump(self.authors, fd, ensure_ascii=False)
 
 
 class AuthorsSpider(scrapy.Spider):
     name = 'authors'
-    custom_settings = {"FEED_FORMAT": "json", "FEED_URI": "authors.json"}
+    custom_settings = {"ITEM_PIPELINES": {Q_Pipline: 100}}
     allowed_domains = ['quotes.toscrape.com']
     start_urls = ['http://quotes.toscrape.com/']
 
     def parse(self, response):
         for quote in response.xpath("/html//div[@class='quote']"):
-            author_response = scrapy.Request(self.start_urls[0][:-1] + quote.xpath("span/a/@href").get())
-            for author_page in author_response.response.xpath("/html//div[@class='author-details']"):
-                print(author_page)
-            #print()
-            #yield {
-            #    "keywords": quote.xpath("div[@class='tags']/a/text()").extract(),
-            #    "author": quote.xpath("span/small/text()").extract(),
-            #    "quote": quote.xpath("span[@class='text']/text()").get()
-            #}
-
-
-class QuotesSpider(scrapy.Spider):
-    name = 'quotes'
-    custom_settings = {"FEED_FORMAT": "json", "FEED_URI": "quotes.json"}
-    allowed_domains = ['quotes.toscrape.com']
-    start_urls = ['http://quotes.toscrape.com/']
-
-    def parse(self, response):
-        for quote in response.xpath("/html//div[@class='quote']"):
-            yield {
-                "keywords": quote.xpath("div[@class='tags']/a/text()").extract(),
-                "author": quote.xpath("span/small/text()").extract(),
-                "quote": quote.xpath("span[@class='text']/text()").get()
-            }
+            keywords = quote.xpath("div[@class='tags']/a/text()").extract()
+            author = quote.xpath("span/small/text()").get().strip()
+            quote_text = quote.xpath("span[@class='text']/text()").get().strip()
+            yield QuoteItem(keywords=keywords, author=author, quote=quote_text)
+            yield response.follow(url=self.start_urls[0] + quote.xpath('span/a/@href').get(),
+                                  callback=self.parse_author)
         next_link = response.xpath("//li[@class='next']/a/@href").get()
         if next_link:
             yield scrapy.Request(url=self.start_urls[0] + next_link)
 
+
+    def parse_author(self, response):
+        author = response.xpath('/html//div[@class="author-details"]')
+        fullname = author.xpath('h3[@class="author-title"]/text()').get().strip()
+        date_born = author.xpath('p/span[@class="author-born-date"]/text()').get().strip()
+        location_born = author.xpath('p/span[@class="author-born-location"]/text()').get().strip()
+        bio = author.xpath('div[@class="author-description"]/text()').get().strip()
+        yield AuthorItem(fullname=fullname, date_born=date_born, location_born=location_born, bio=bio)
+
+
 def load_json():
-    with open('json-files/authors.json', 'r', encoding='utf-8') as fh:
+    with open('authors.json', 'r', encoding='utf-8') as fh:
         rez = json.load(fh)
         for itr in rez:
             new_author = Author()
@@ -52,7 +87,7 @@ def load_json():
             new_author.fullname = itr['fullname']
             new_author.save()
 
-    with open('json-files/qoutes.json', 'r', encoding='utf-8') as fh:
+    with open('quotes.json', 'r', encoding='utf-8') as fh:
         rez = json.load(fh)
         for itr in rez:
             authors = Author.objects(fullname=itr['author'])
@@ -64,9 +99,9 @@ def load_json():
             new_quote.save()
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     process = CrawlerProcess()
     process.crawl(AuthorsSpider)
     process.start()
-
+    #from 8 HW
+    load_json()
